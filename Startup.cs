14a6116d4d0c1c4168249bc82b22015952f6025d
kizymatzenbacher <kizymatzenbacher@gmail.com>
2023-04-11 12:25:57 -0600

    commit

--- Startup.cs
+++ Startup.cs
@@ -34,6 +34,7 @@ namespace INTEX
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
